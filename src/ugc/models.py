8f5aa76796597766e69a96cf3f84417d8ae0ce55
Albert Iost <albertiost@mail.ru>
2023-02-04 19:03:50 +0300

    Add Profile and Message models

--- src/ugc/models.py
+++ src/ugc/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    external_id = models.PositiveIntegerField(
+        verbose_name='User ID in telegram'
+    )
+    name = models.TextField(
+        verbose_name='Username'
+    )
+
+    def __str__(self):
+        return f'{self.name}#{self.external_id}'
+    class Meta:
+        verbose_name = 'Profile'
+
+class Message(models.Model):
+    profile = models.ForeignKey(
+        to='ugc.Profile',
+        verbose_name='Profile',
+        on_delete=models.PROTECT,
+    )
+    text = models.TextField(
+        verbose_name='Text',
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Time of receipt',
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f'Message {self.pk} from {self.profile}'
+
+    class Meta:
+        verbose_name = 'Message'
