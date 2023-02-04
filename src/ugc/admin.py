ed99b4a0900d035966f41586cd41f2dc42d83f1b
Albert Iost <albertiost@mail.ru>
2023-02-04 19:02:23 +0300

    Register Profile and Message models in admin panel

--- src/ugc/admin.py
+++ src/ugc/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .forms import ProfileForm
+from .models import Profile
+from .models import Message
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'external_id', 'name')
+    form = ProfileForm
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'profile', 'text', 'created_at')
