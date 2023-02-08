d032b87e4e4c97728dce12c9747300ca57b3afb5
Albert Iost <albertiost@mail.ru>
2023-02-08 16:43:55 +0300

    Add telegram bot like management command

---
+++ src/tgbot/handlers/commands.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.exceptions import ValidationError
+from telegram import Update
+from telegram.ext import ContextTypes
+from ugc.shortener import Shortener
+from ugc.models import Profile
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    user, created = await Profile.objects.aget_or_create(external_id=chat_id)
+    text = "I'm a bot, please talk to me!"
+    await context.bot.send_message(chat_id=chat_id, text=text)
+
+async def cut(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    args = context.args
+    chat_id = update.effective_chat.id
+    user, created = await Profile.objects.aget_or_create(external_id=chat_id)
+    try:
+        link = await Shortener(user).cut_link(args[0])
+        text = settings.HOST + '/' + link.token
+        await context.bot.send_message(chat_id=chat_id, text=text)
+    except (ValidationError, IndexError):
+        await context.bot.send_message(chat_id=chat_id,
+                                       text='Your URL is incorrect. Please, send the correct URL address.')
---
+++ src/tgbot/management/commands/bot.py
@@ -0,0 +1,23 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from tgbot.handlers.commands import *
+class Command(BaseCommand):
+    help = 'Implemented to Django application telegram bot setup command'
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    def handle(self, *args, **options):
+        app = ApplicationBuilder().token(settings.TOKEN).build()
+
+        start_handler = CommandHandler('start', start)
+        cut_handler = CommandHandler('cut', cut)
+
+        app.add_handler(start_handler)
+        app.add_handler(cut_handler)
+
+        app.run_polling()
