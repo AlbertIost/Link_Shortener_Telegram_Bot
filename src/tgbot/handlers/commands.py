26240540c14891087f33d32b47362cf59768a048
Albert Iost <albertiost@mail.ru>
2023-02-08 16:58:55 +0300

    Delete Site import

--- src/tgbot/handlers/commands.py
+++ src/tgbot/handlers/commands.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from telegram import Update
 from telegram.ext import ContextTypes
