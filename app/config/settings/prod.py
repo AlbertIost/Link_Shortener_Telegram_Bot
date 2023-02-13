bd51e1f4ea50b5264a71cfcacd13e7b2e5af9ba0
Albert Iost <albertiost@mail.ru>
2023-02-13 22:51:54 +0300

    Change settings for pythonanywhere

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -8,7 +8,11 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '3306',
     }
 }
