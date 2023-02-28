4d48c9f226fa9276bd7ed06f2740631d62f6a586
Albert Iost <albertiost@mail.ru>
2023-02-28 15:23:34 +0300

    Added a check on the search robot and TelegramBot so as not to count their view page

--- app/ugc/views.py
+++ app/ugc/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.views.decorators.csrf import csrf_exempt
 
 from ugc.models import Link
@@ -8,11 +10,18 @@ from .utils import *
 # Create your views here.
 
 def click_on_short_link(request, link_token):
+    crawlers = [
+        'google', 'yandex', 'baiduspider', 'lycos', 'genieo', 'slurp', 'webalta', 'facebook',
+        'mail.Ru', 'ia_archiver', 'teoma', 'yahoo', 'ask', 'rambler', 'crawler4j', 'mj12',
+        'seznam', 'bot', 'curl', 'duckduckgo', 'aol', 'lighthouse', 'telegram'
+    ]
+    pattern = '|'.join(crawlers)
     try:
         link = Link.objects.get(token=link_token)
         response = redirect(link.original_link)
 
-        register_click_on_link(link, get_client_ip(request))
+        if not re.search(pattern, str(request.META['HTTP_USER_AGENT']).lower()):
+            register_click_on_link(link, get_client_ip(request))
 
         return response
     except:
